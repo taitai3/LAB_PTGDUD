3a31774244dd1a194ce65972adb7911674b891db
H82M19\Student <Student>
2026-04-11 14:08:30 +0700

    bai2 Not fount

--- Tuan5/src/App.jsx
+++ Tuan5/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import NotFound from './components/NotFound'
 import './App.css'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
